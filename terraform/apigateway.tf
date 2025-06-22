074a9d39c19ead3f74570ed3154a87499ec31853
Jay Patel <jay21102004patel@gmail.com>
2025-06-22 23:08:23 +0530

    Updated apigw.tf

--- terraform/apigateway.tf
+++ terraform/apigateway.tf
@@ -3,7 +3,7 @@ resource "aws_apigatewayv2_api" "doc_vault_api" {
   protocol_type = "HTTP"
 
   cors_configuration {
-    allow_origins     = ["*"] # Change to your actual frontend URL in production
+    allow_origins     = ["*"]
     allow_methods     = ["GET", "POST", "OPTIONS"]
     allow_headers     = ["Content-Type", "Authorization"]
     expose_headers    = ["*"]
@@ -12,38 +12,27 @@ resource "aws_apigatewayv2_api" "doc_vault_api" {
 }
 
 resource "aws_apigatewayv2_integration" "lambda_integration" {
-  api_id           = aws_apigatewayv2_api.doc_vault_api.id
-  integration_type = "AWS_PROXY"
-  integration_uri  = aws_lambda_function.doc_vault_lambda.invoke_arn
-  integration_method = "POST"
+  api_id                 = aws_apigatewayv2_api.doc_vault_api.id
+  integration_type       = "AWS_PROXY"
+  integration_uri        = aws_lambda_function.doc_vault_lambda.invoke_arn
+  integration_method     = "POST"
   payload_format_version = "2.0"
 }
 
-resource "aws_apigatewayv2_route" "register_route" {
-  api_id    = aws_apigatewayv2_api.doc_vault_api.id
-  route_key = "POST /register"
-  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
-}
-
-resource "aws_apigatewayv2_route" "login_route" {
-  api_id    = aws_apigatewayv2_api.doc_vault_api.id
-  route_key = "POST /login"
-  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
-}
-
-resource "aws_apigatewayv2_route" "list_files_route" {
-  api_id    = aws_apigatewayv2_api.doc_vault_api.id
-  route_key = "GET /list-files"
-  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+resource "aws_apigatewayv2_route" "routes" {
+  for_each   = toset(var.api_routes)
+  api_id     = aws_apigatewayv2_api.doc_vault_api.id
+  route_key  = "${each.key == "/list-files" ? "GET" : "POST"} ${each.value}"
+  target     = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
 }
 
 resource "aws_apigatewayv2_stage" "default" {
   api_id      = aws_apigatewayv2_api.doc_vault_api.id
-  name        = "default"
+  name        = "$default"
   auto_deploy = true
 }
 
-resource "aws_lambda_permission" "allow_apigw_invoke" {
+resource "aws_lambda_permission" "apigw_permission" {
   statement_id  = "AllowAPIGatewayInvoke"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.doc_vault_lambda.function_name
